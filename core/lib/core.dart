b8991a7dead6656631419a9b1655da64d2073e89
Vladislav Kirichuk <vlad.kirichuk50@gmail.com>
2023-11-12 18:41:01 +0300

    Create Modular project structure.

---
+++ core/lib/core.dart
@@ -0,0 +1,11 @@
+library core;
+
+export 'package:hive/hive.dart';
+export 'package:hive_flutter/hive_flutter.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:connectivity_plus/connectivity_plus.dart';
+export 'package:dio/dio.dart';
+export 'package:retrofit_generator/retrofit_generator.dart';
+export 'package:json_annotation/json_annotation.dart';
+export 'package:json_serializable/json_serializable.dart';
+export 'package:equatable/equatable.dart';
